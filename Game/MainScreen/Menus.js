b504fd1336555fce71b31c5b213ac42fab7db71d
Travis Miller <cionze@hotmail.com>
2013-09-23 22:25:10 -0600

    push

--- Game/MainScreen/Menus.js
+++ Game/MainScreen/Menus.js
@@ -12,7 +12,7 @@ function CreateMenus(Width, Height) {
 	  var MenuHeight = Canvas.height * 0.069;
 	  var MenuYSpace = Canvas.height * 0.093; //menuboxes /end
 	  
-	  this.stringT = new advancedString("RedNoToolTip`red` Orange/ToolTip/ToopTipBoxColoredBlue`orange`&This tooltip is for the blue word& Pink`pink` Purple`Purple` gray`gray` white`white` green`green` yellow`yellow`");
+	  this.stringT = new advancedString("We&a& should make a font-class,`red` it could be like var`blue` font{size:`blue` 16,`blue` font:`blue` 16px`blue` newfont}`blue`");
 	  
 	  
 	 
